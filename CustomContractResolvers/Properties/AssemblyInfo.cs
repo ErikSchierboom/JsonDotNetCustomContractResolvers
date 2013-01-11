37daa78e40211e51f905d43c7d91280758e7ab87
Erik Schierboom <erik_schierboom@hotmail.com>
2013-01-11 09:54:43 +0100

    Allow package restore

--- CustomContractResolvers/Properties/AssemblyInfo.cs
+++ CustomContractResolvers/Properties/AssemblyInfo.cs
@@ -1,18 +1,19 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CustomContractResolvers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("JSON.NET Custom Contract Resolvers")]
+[assembly: AssemblyDescription("Custom contract resolvers for JSON.NET, including a contract resolver with which one can easily exclude fields from being serialized without using attributes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomContractResolvers")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Erik Schierboom")]
+[assembly: AssemblyProduct("JsonDotNet.CustomContractResolvers")]
+[assembly: AssemblyCopyright("Copyright © Erik Schierboom 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
