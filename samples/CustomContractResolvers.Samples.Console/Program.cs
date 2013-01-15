d31c67f1938950eff0589fc53c53fcbd629156d5
Erik Schierboom <erik_schierboom@hotmail.com>
2013-01-15 16:18:41 +0100

    added console sample project

---
+++ samples/CustomContractResolvers.Samples.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace JsonDotNet.CustomContractResolvers.Samples.Console
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            ShowExplanation();
+            ShowSimpleUsage();
+            ShowPropertyWildcardUsage();
+            ShowTypeWildcardUsage();
+            ShowGeneralWildcardUsage();
+        }
+
+        private static void ShowExplanation()
+        {
+            Console.WriteLine("This console application will show how the CustomPropertyContractResolver class can be used.");
+        }
+
+        private static void ShowSimpleUsage()
+        {
+            // TODO: show example
+        }
+
+        private static void ShowPropertyWildcardUsage()
+        {
+            // TODO: show example
+        }
+
+        private static void ShowTypeWildcardUsage()
+        {
+            // TODO: show example
+        }
+
+        private static void ShowGeneralWildcardUsage()
+        {
+            // TODO: show example
+        }
+    }
+}
