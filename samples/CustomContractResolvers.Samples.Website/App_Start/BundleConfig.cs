4e549fd608aec6dd09b061f1303075c7c73d1f70
Erik Schierboom <erik_schierboom@hotmail.com>
2013-01-30 16:37:43 +0100

    Added first version of Website sample project

---
+++ samples/CustomContractResolvers.Samples.Website/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace JsonDotNet.CustomContractResolvers.Samples.Website.App_Start
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+        }
+    }
+}
---
+++ samples/CustomContractResolvers.Samples.Website/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿namespace JsonDotNet.CustomContractResolvers.Samples.Website.Controllers
+{
+    using System.Web.Mvc;
+
+    using JsonDotNet.CustomContractResolvers;
+    using JsonDotNet.CustomContractResolvers.Samples.Website.ViewModels.Home;
+
+    using Newtonsoft.Json;
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new IndexViewModel();
+
+            // Use a default JSON string
+            model.Json = "{\"Id\":12,\"Title\":\"Inception\",\"Director\":\"Christopher Nolan\"}";
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                // Create a PropertiesContractResolver instance using the specified properties
+                // and exclude properties values
+                var propertiesContractResolver = new PropertiesContractResolver();
+                propertiesContractResolver.Properties.Add(model.Properties);
+                propertiesContractResolver.ExcludeProperties.Add(model.ExcludeProperties);
+
+                // Create a JsonSerializerSettings instance that uses our PropertiesContractResolver instance
+                // to resolve contracts (thereby influencing the generated JSON)
+                var serializerSettings = new JsonSerializerSettings { ContractResolver = propertiesContractResolver };
+
+                // Create an object from the specified JSON
+                var deserializeObject = JsonConvert.DeserializeObject(model.Json);
+
+                // Convert the object using our PropertiesContractResolver instance 
+                model.JsonSerializationResult = JsonConvert.SerializeObject(deserializeObject, serializerSettings);
+            }
+
+            return this.View(model);
+        }
+    }
+}
